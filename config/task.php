863bd84bcf1ec3990424f1d336189732ac439649
Matias Camiletti <matias.camiletti@gmail.com>
2023-06-19 17:17:34 -0300

    Add config file.

---
+++ config/task.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'project_id' => env('CLOUD_TASK_PROJECT_ID', ''),
+    'location_id' => env('CLOUD_TASK_LOCATION_ID', ''),
+    'queue_id' => env('CLOUD_TASK_QUEUE_ID', ''),
+    'secret_key' => env('CLOUD_TASK_SECRET_KEY', ''),
+];
