69d3df01c27d296d6587dc06c40ebdaa9f1d9c6a
Matias Camiletti <matias.camiletti@gmail.com>
2023-06-19 17:47:10 -0300

    Add task controller.

---
+++ src/Http/Controllers/TaskController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Tots\CloudTask\Http\Controller;
+
+use Illuminate\Http\Request;
+use Tots\CloudTask\Services\TaskService;
+
+class TaskController extends \Laravel\Lumen\Routing\Controller
+{
+    /**
+     *
+     * @var TaskService
+     */
+    protected $service;
+
+    public function __construct(TaskService $service)
+    {
+        $this->service = $service;
+    }
+
+    public function handle(Request $request)
+    {
+        // Validation
+        $this->validate($request, [
+            'secret_key' => 'required',
+            'tots_task_name' => 'required'
+        ]);
+        // Validate is Secret key is correct
+        if (!$this->service->isValidSecretKey($request->input('secret_key'))) {
+            throw new \Exception('The secret key is incorrect');
+        }
+        // Get Task Class Name
+        $taskName = $request->input('tots_task_name');
+        // Create Task
+        $task = new $taskName();
+        // Run Task
+        $task->run($request->all());
+        // Return
+        return true;
+    }
+}
